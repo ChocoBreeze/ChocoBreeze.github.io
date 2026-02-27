c760038bc7fac29fe93720079f3f657a2d990244
ChocoBreeze <ds030337@gmail.com>
2026-02-28 00:54:00 +0900

    feat: add Table of Contents and search functionality with header UI improvements

---
+++ src/pages/search.json.ts
@@ -0,0 +1,15 @@
+import { getCollection } from 'astro:content';
+
+export async function GET() {
+	const posts = await getCollection('blog');
+	const searchIndex = posts.map((post) => ({
+		title: post.data.title,
+		description: post.data.description,
+		categories: post.data.categories,
+		slug: post.id,
+	}));
+
+	return new Response(JSON.stringify(searchIndex), {
+		headers: { 'Content-Type': 'application/json' },
+	});
+}
