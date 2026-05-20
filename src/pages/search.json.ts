6477a58399fb7f43156275c1b226b497bbfaa0cb
ChocoBreeze <ds030337@gmail.com>
2026-05-20 23:47:14 +0900

    Expand search index coverage

--- src/pages/search.json.ts
+++ src/pages/search.json.ts
@@ -1,13 +1,21 @@
-import { getCollection } from 'astro:content';
+import { getCollection, render } from 'astro:content';
 
 export async function GET() {
 	const posts = await getCollection('blog');
-	const searchIndex = posts.map((post) => ({
-		title: post.data.title,
-		description: post.data.description,
-		categories: post.data.categories,
-		slug: post.data.slug || post.id,
-	}));
+	const searchIndex = await Promise.all(
+		posts.map(async (post) => {
+			const { headings } = await render(post);
+
+			return {
+				title: post.data.title,
+				description: post.data.description,
+				categories: post.data.categories,
+				tags: post.data.tags ?? [],
+				headings: headings.map((heading) => heading.text),
+				slug: post.data.slug || post.id,
+			};
+		}),
+	);
 
 	return new Response(JSON.stringify(searchIndex), {
 		headers: {
